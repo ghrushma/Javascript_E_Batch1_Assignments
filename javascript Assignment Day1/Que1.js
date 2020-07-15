e813869a3aea2d0f09f56408384a525a8887dad6
ghrushma <68225963+ghrushma@users.noreply.github.com>
2020-07-15 13:52:56 +0530

    Add files via upload

---
+++ javascript Assignment Day1/Que1.js
@@ -0,0 +1,29 @@
+//Question1Day1_14july
+
+// alert("Click me to Proceed");
+console.log("hey I am a javascript code");
+
+// Comments in javascript
+// Single line comments are written using //
+// Multine Comments are written using /* */
+
+/*console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");*/
+
+var a = {name:"Ghrushma", age:"26", city:"UK"}
+var b = {name:"John", age:"28", city:"London"}
+var c = {name:"Susen", age:"26", city:"Mexico"}
+
+console.time('Time taken');
+console.log("Hey")
+console.error("an error occured!");
+console.warn("Hey this is a warning!!");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log({a ,b ,c})
+console.table({a ,b ,c})
+console.log('%c Custom message','color:red;')
+console.timeEnd('Time taken ended');
---
+++ javascript Assignment Day1/que02.js
@@ -0,0 +1,56 @@
+// Var
+// String
+var name = "Ghrushma"; 
+// Number
+var age = 25;
+// Boolean
+var canFly = false;
+// Array
+var languages = ['Hindi','English','Marathi'];
+// Objects
+var friends = {
+    name:"Kavita",
+    hobby:"Drawing",
+}
+
+var a = 10;
+console.log(a);
+
+var a = null;
+console.log(a);
+
+// Let 
+{
+   let city;
+   let name = "Priya"; 
+   name = "Riva";
+   console.log("Using Let: ", name);
+}
+console.log(name)
+
+// const - Constant
+
+const country = "India";
+console.log(country);
+
+// country = "USA";
+
+const _fruits = ["mango", "apple"]
+console.log(_fruits)
+_fruits.push('banana');
+console.log(_fruits)
+
+
+/* Naming a variable
+1. variable name cannot start with a number
+2. It can start with letters, $, _.
+3. $ is mainly used in JQuery
+4. _  is used to declare private variables [OOPS in Javscript]
+*/
+
+/*
+1. camelCase ************
+2. kebab-case
+3. snake_case
+4. PascalCase
+*/
